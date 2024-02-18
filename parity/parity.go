bac9f96f6c51e31ea602695b6240c3efdc90876c
Kevin Wang <kevin@xorkevin.com>
2024-02-17 23:40:31 -0800

    Impl parity packet header

---
+++ parity/parity.go
@@ -0,0 +1,100 @@
+package parity
+
+import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
+
+	"xorkevin.dev/kerrors"
+)
+
+var (
+	// ErrShortHeader is returned when the provided data is short
+	ErrShortHeader errShortHeader
+	// ErrMalformedHeader is returned when the header is malformed
+	ErrMalformedHeader errHeader
+)
+
+type (
+	errShortHeader struct{}
+	errHeader      struct{}
+)
+
+func (e errShortHeader) Error() string {
+	return "Short header"
+}
+
+func (e errHeader) Error() string {
+	return "Malformed header"
+}
+
+type (
+	PacketHeader struct {
+		Version    uint32
+		headerSum  uint32
+		PacketHash [32]byte
+		Length     uint64
+		Kind       uint32
+	}
+)
+
+const MagicBytes = "bcsp\x0d\x0a\x1a\x04"
+
+func (h *PacketHeader) MarshalBinary() ([]byte, error) {
+	res := make([]byte, 60)
+	copy(res, []byte(MagicBytes))
+	binary.BigEndian.PutUint32(res[8:], h.Version)
+	binary.BigEndian.PutUint32(res[12:], h.Sum())
+	copy(res[16:], h.PacketHash[:])
+	binary.BigEndian.PutUint64(res[48:], h.Length)
+	binary.BigEndian.PutUint32(res[56:], h.Kind)
+	return res, nil
+}
+
+func (h *PacketHeader) UnmarshalBinary(data []byte) error {
+	if len(data) < 12 {
+		return kerrors.WithKind(nil, ErrShortHeader, "Short header")
+	}
+	if !bytes.Equal(data[:8], []byte(MagicBytes)) {
+		return kerrors.WithKind(nil, ErrMalformedHeader, "Invalid magic bytes")
+	}
+	{
+		v := binary.BigEndian.Uint32(data[8:12])
+		if v != 0 {
+			return kerrors.WithKind(nil, ErrMalformedHeader, "Invalid version")
+		}
+		h.Version = v
+	}
+	if len(data) < 60 {
+		return kerrors.WithKind(nil, ErrShortHeader, "Short header")
+	}
+	h.headerSum = binary.BigEndian.Uint32(data[12:16])
+	copy(h.PacketHash[:], data[16:])
+	h.Length = binary.BigEndian.Uint64(data[48:56])
+	h.Kind = binary.BigEndian.Uint32(data[56:60])
+	return nil
+}
+
+func (h *PacketHeader) Sum() uint32 {
+	t := crc32.MakeTable(crc32.Castagnoli)
+	var n [8]byte
+	binary.BigEndian.PutUint32(n[:], h.Version)
+	s := crc32.Checksum(n[:4], t)
+	s = crc32.Update(s, t, h.PacketHash[:])
+	binary.BigEndian.PutUint64(n[:], h.Length)
+	s = crc32.Update(s, t, n[:])
+	binary.BigEndian.PutUint32(n[:], h.Kind)
+	s = crc32.Update(s, t, n[:4])
+	return s
+}
+
+func (h *PacketHeader) Verify() error {
+	if h.Sum() != h.headerSum {
+		return kerrors.WithKind(nil, ErrMalformedHeader, "Invalid checksum")
+	}
+	return nil
+}
+
+func (h *PacketHeader) SetSum() {
+	h.headerSum = h.Sum()
+}
---
+++ parity/parity_test.go
@@ -0,0 +1,31 @@
+package parity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCensus(t *testing.T) {
+	t.Parallel()
+
+	assert := require.New(t)
+
+	header := PacketHeader{
+		Version:    0,
+		PacketHash: [32]byte{0, 1, 2, 3, 4, 5, 6, 7},
+		Length:     8,
+		Kind:       1,
+	}
+
+	headerBytes, err := header.MarshalBinary()
+	assert.NoError(err)
+
+	header.SetSum()
+	assert.NoError(header.Verify())
+
+	var target PacketHeader
+	assert.NoError(target.UnmarshalBinary(headerBytes))
+	assert.Equal(header, target)
+	assert.NoError(target.Verify())
+}
